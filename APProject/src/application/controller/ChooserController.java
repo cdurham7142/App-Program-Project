63f1ba91f3c35567908fc0bb6cfe70c35d49f349
jakedeloss <56040649+jakedeloss@users.noreply.github.com>
2019-12-06 18:34:57 -0600

    Add files via upload

--- APProject/src/application/controller/ChooserController.java
+++ APProject/src/application/controller/ChooserController.java
@@ -1,60 +1,114 @@
 package application.controller;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
+import application.model.CustomerData;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-/**
- * This class will represent the ChooserController which will implement the EventHandler
- * interface, and any events that occur when the user interacts with MainScene.fxml.
- *
- * @authors: Jacob De Los Santos bfn715
- * 	     Colin Durham sif159
- * UTSA CS 3443 - Group Project
- * Fall 2019
- * 
- */
 public class ChooserController {
 
-	private final static String[] names = {"Eskimo","Camoflage","Stripped","Silk","Desert","Ogee","Zebra","Swirl","Hexagon"};
     @FXML
-    private static GridPane grid;
-    
-    private static boolean loadedImages = false;
-    
-    @FXML
-    private Button confirmButton;
+    private Button tundraButton;
 
     @FXML
-    void onConfirm(ActionEvent event) throws Exception {
-    	Stage stage = (Stage) confirmButton.getScene().getWindow();
-    	Parent root = FXMLLoader.load(getClass().getResource("/application/view/MainScene.fxml"));
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-    public static void loadImages() {
-    	if (loadedImages)
-    		return;
-    	File[] files = new File("scrunchies").listFiles();
-    	List<Image> iArr = new ArrayList<Image>();
-    	for (File file : files)
-    		iArr.add(new Image(file.getAbsolutePath()));
-    	for (int i = 0; i < files.length; i++)
-    		grid.addRow(i, new ImageView(iArr.get(i)), new Label(names[i]));
-    	loadedImages = true;
+    private Button eskimoButton;
+
+    @FXML
+    private Button stripedButton;
+
+    @FXML
+    private Button dottedButton;
+
+    @FXML
+    private Button camoflageButton;
+
+    @FXML
+    private Button zebraButton;
+
+    @FXML
+    private Button hexagonButton;
+
+    @FXML
+    private Button desertButton;
+
+    @FXML
+    void handle(ActionEvent event) throws Exception {
+    	Parent root;
+    	Stage stage;
+    	CustomerData alotOfScrunch2019 = CustomerData.getInstance();
+    	alotOfScrunch2019.patternSelection = "";
+    	if(event.getSource()== tundraButton){
+    		alotOfScrunch2019.patternSelection = "Tundra";
+    		stage = (Stage) tundraButton.getScene().getWindow();
+            root = (Parent) FXMLLoader.load(getClass().getResource("/application/view/DesignScene.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();  		
+    	}
+    	if(event.getSource()== eskimoButton){
+    		alotOfScrunch2019.patternSelection = "Eskimo";
+    		stage = (Stage)eskimoButton.getScene().getWindow();
+            root = (Parent) FXMLLoader.load(getClass().getResource("/application/view/DesignScene.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();  		
+    	}
+    	if(event.getSource()== desertButton){
+    		alotOfScrunch2019.patternSelection = "Desert";
+    		stage = (Stage) desertButton.getScene().getWindow();
+            root = (Parent) FXMLLoader.load(getClass().getResource("/application/view/DesignScene.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();  		
+    	}
+    	if(event.getSource()== stripedButton){
+    		alotOfScrunch2019.patternSelection = "Striped";
+    		stage = (Stage) stripedButton.getScene().getWindow();
+            root = (Parent) FXMLLoader.load(getClass().getResource("/application/view/DesignScene.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();  		
+    	}
+    	if(event.getSource()== camoflageButton){
+    		alotOfScrunch2019.patternSelection = "Camoflage";
+    		stage = (Stage) camoflageButton.getScene().getWindow();
+            root = (Parent) FXMLLoader.load(getClass().getResource("/application/view/DesignScene.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();  		
+    	}
+    	if(event.getSource()== hexagonButton){
+    		alotOfScrunch2019.patternSelection = "Hexagon";
+    		stage = (Stage) hexagonButton.getScene().getWindow();
+            root = (Parent) FXMLLoader.load(getClass().getResource("/application/view/DesignScene.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();  		
+    	}
+    	if(event.getSource()== dottedButton){
+    		alotOfScrunch2019.patternSelection = "Dotted";
+    		stage = (Stage) dottedButton.getScene().getWindow();
+            root = (Parent) FXMLLoader.load(getClass().getResource("/application/view/DesignScene.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();  		
+    	}
+    	if(event.getSource()== zebraButton){
+    		alotOfScrunch2019.patternSelection = "Zebra";
+    		stage = (Stage) zebraButton.getScene().getWindow();
+            root = (Parent) FXMLLoader.load(getClass().getResource("/application/view/DesignScene.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();  		
+    	}
+    	System.out.println(alotOfScrunch2019.patternSelection);
+    	
+
     }
 
 }
+
